6d2d7bc39dbedb4d14e5cbe68479212449a8ebbe
abdurasulov-olqar <abdurasulovolqar123@gmail.com>
2023-03-07 18:11:16 +0500

    name

--- name.py
+++ name.py
@@ -14,7 +14,16 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        url = self.base_url + 'Name'
+        payload = {
+            "nameType": nameType,
+            "quantity": quantity
+        }
+        headers = {
+            "X-Api-Key": api_key
+        }
+        response = requests.get(url, params = payload, headers = headers)
+        return response.json()
     
     def get_name_suggestions(self, api_key: str, startingWords: str) -> list:
         '''get name suggestions
@@ -26,7 +35,15 @@ class Name(Randommer):
         Returns:
             list: list of name suggestions
         '''
-        pass
+        url = self.base_url + 'Name/Suggestions'
+        payloads = {
+            "startingWords": startingWords
+        }
+        headers = {
+            "X-Api-Key": api_key
+        }
+        response = requests.get(url, params = payloads, headers = headers)
+        return response.json()
     
     def get_name_cultures(self, api_key: str) -> list:
         '''get available cultures
@@ -37,4 +54,13 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        url = self.base_url + 'Name/Cultures'
+        headers = {
+            "X-Api-Key": api_key
+        }
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+name = Name()
+# print(name.get_name(nameType='firstname', quantity = 12, api_key='2d794c6f46094ceb96bd719c1c26c984'))
+print(name.get_name_suggestions("2d794c6f46094ceb96bd719c1c26c984","ali"))
